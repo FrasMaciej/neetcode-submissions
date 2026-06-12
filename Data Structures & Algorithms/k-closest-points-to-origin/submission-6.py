90f17e91212f09f44809900a6fb147083738abcf
Maciej Fras <37934674+FrasMaciej@users.noreply.github.com>
2026-06-12 22:22:29 +0200

    Add: k-closest-points-to-origin - submission-6

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-6.py
@@ -0,0 +1,34 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        min_heap = []
+        heapq.heapify(min_heap)
+
+        for x, y in points:
+            distance = (0 - x) ** 2 + (0 - y) ** 2
+            heapq.heappush(min_heap, (distance, [x, y]))
+
+        closest_points = []
+        for i in range(k):
+            closest_points.append(heapq.heappop(min_heap)[1])
+        
+        return closest_points
+
+        # Notes:
+
+        # points[n][0] -- x
+        # points[n][1] -- y
+
+        # for two points Euclidean distance is defined as:
+        # sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+        # brute force: 1. calculate all distances and add to list, 2. sort distances, 3. return k points
+        
+        # better: store k of the closest points in max heap
+        # when smaller than the max of the heap found then use replace it on heap
+
+        # method should return points coords, but heap is storing distances. 
+        # Need to use tuple to handle that
+
+
+
+        
